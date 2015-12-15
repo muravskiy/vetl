5c607a4f8e40c926e45c4ff81fe57d600cfe3326
muravskiy <i.muravskiy@gmail.com>
2015-12-15 19:15:48 +0200

    Изменил логин и пароль

--- models/User.php
+++ models/User.php
@@ -13,8 +13,8 @@ class User extends \yii\base\Object implements \yii\web\IdentityInterface
     private static $users = [
         '100' => [
             'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
+            'username' => 'root',
+            'password' => '5733834',
             'authKey' => 'test100key',
             'accessToken' => '100-token',
         ],
