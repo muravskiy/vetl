7cb0de2a0bed362053aa12dcb8820df475d8aba7
muravskiy <i.muravskiy@gmail.com>
2015-12-17 22:37:21 +0200

    Исправил название базы

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=vetl',
     'username' => 'root',
     'password' => '5733834',
     'charset' => 'utf8',
