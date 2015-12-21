64475a7c712d9567802ca35e5890eccaa1c60718
Oleg <tempblackstar@gmail.com>
2015-12-21 15:41:16 +0200

    Добавил вид добавления начальника теперь нужно сделать логику на php mysql что работало а пока только вид

---
+++ views/admin/addnachkar.php
@@ -0,0 +1,14 @@
+<?php
+/* @var $this yii\web\View */
+?>
+<h1>Наверное тут будет меню для выбора всяких штук</h1>
+
+<form>
+	Имя нового дорогого и мудимого начальника.<br/>
+	<input name="text" value="Bos Name" type="text"><br/>
+	<input name="submit" value="Изменить" type="submit">
+</form>
+
+<p>
+    <code>file: <?= __FILE__; ?></code>.
+</p>
