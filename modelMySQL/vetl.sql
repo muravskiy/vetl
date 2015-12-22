9bb3dadad4cbf091df6cfdae6dbd63f8b2d31bb7
muravskiy <i.muravskiy@gmail.com>
2015-12-22 22:28:23 +0200

    Исправил кракозябры в базе

--- modelMySQL/vetl.sql
+++ modelMySQL/vetl.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10.10
+-- version 4.4.13.1deb1
 -- http://www.phpmyadmin.net
 --
--- Хост: 127.0.0.1:3306
--- Время создания: Дек 22 2015 г., 15:33
--- Версия сервера: 5.5.45
--- Версия PHP: 5.6.12
+-- Хост: localhost
+-- Время создания: Дек 22 2015 г., 22:27
+-- Версия сервера: 5.6.27-0ubuntu1
+-- Версия PHP: 5.6.11-1ubuntu3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- База данных: `vetl`
@@ -27,11 +27,10 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `brigada` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL,
   `room_brigade` varchar(45) NOT NULL,
-  `last_name_SHNS` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+  `last_name_SHNS` varchar(45) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `brigada`
@@ -48,14 +47,13 @@ INSERT INTO `brigada` (`id`, `room_brigade`, `last_name_SHNS`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `certificate` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `issued` varchar(254) NOT NULL,
   `name_certificate` varchar(254) NOT NULL,
   `number_certificate` varchar(45) NOT NULL,
   `date_issue_certificate` date NOT NULL,
-  `valid_to_certificate` date NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=16 ;
+  `valid_to_certificate` date NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `certificate`
@@ -75,26 +73,18 @@ INSERT INTO `certificate` (`id`, `issued`, `name_certificate`, `number_certifica
 --
 
 CREATE TABLE IF NOT EXISTS `facility` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL,
   `facility` varchar(45) NOT NULL,
-  `hazard_category_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_tbl_facility_tbl_hazard_category_idx` (`hazard_category_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+  `hazard_category_id` int(10) unsigned NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `facility`
 --
 
 INSERT INTO `facility` (`id`, `facility`, `hazard_category_id`) VALUES
-(1, 'Р РµР»РµР№РЅРµ РїСЂРёРјС–С‰РµРЅРЅСЏ', 1),
-(2, 'РњР°РЅРµРІСЂРѕРІР° Р’РёС€РєР°', 1),
-(3, 'Р РµР»РµР№РЅРµ РїСЂРёРјС–С‰РµРЅРЅСЏ', 1),
-(4, 'РњР°РЅРµРІСЂРѕРІР° Р’РёС€РєР°', 1),
-(5, 'Р РµР»РµР№РЅРµ РїСЂРёРјС–С‰РµРЅРЅСЏ', 1),
-(6, 'РњР°РЅРµРІСЂРѕРІР° Р’РёС€РєР°', 1),
-(7, 'Р РµР»РµР№РЅРµ РїСЂРёРјС–С‰РµРЅРЅСЏ', 1),
-(8, 'РњР°РЅРµРІСЂРѕРІР° Р’РёС€РєР°', 1);
+(3, 'Релейне приміщення', 1),
+(4, 'Маневрова Вишка', 1);
 
 -- --------------------------------------------------------
 
@@ -103,24 +93,17 @@ INSERT INTO `facility` (`id`, `facility`, `hazard_category_id`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `hazardcategory` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `hazard_category` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+  `id` int(10) unsigned NOT NULL,
+  `hazard_category` varchar(45) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `hazardcategory`
 --
 
 INSERT INTO `hazardcategory` (`id`, `hazard_category`) VALUES
-(1, 'РќРµР±РµР·РїРµС‡РЅС–'),
-(2, 'РџС–РґРІРёС‰РµРЅРЅРѕС— РЅРµР±РµР·РїРµРєРё'),
-(3, 'РќРµР±РµР·РїРµС‡РЅС–'),
-(4, 'РџС–РґРІРёС‰РµРЅРЅРѕС— РЅРµР±РµР·РїРµРєРё'),
-(5, 'РќРµР±РµР·РїРµС‡РЅС–'),
-(6, 'РџС–РґРІРёС‰РµРЅРЅРѕС— РЅРµР±РµР·РїРµРєРё'),
-(7, 'РќРµР±РµР·РїРµС‡РЅС–'),
-(8, 'РџС–РґРІРёС‰РµРЅРЅРѕС— РЅРµР±РµР·РїРµРєРё');
+(1, 'Небезпечні'),
+(2, 'Підвищенної небезпеки');
 
 -- --------------------------------------------------------
 
@@ -129,21 +112,19 @@ INSERT INTO `hazardcategory` (`id`, `hazard_category`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `locality` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL,
   `locality` varchar(45) NOT NULL,
-  `type_locality_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_tbl_locality_tbl_type_locality_idx` (`type_locality_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+  `type_locality_id` int(10) unsigned NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `locality`
 --
 
 INSERT INTO `locality` (`id`, `locality`, `type_locality_id`) VALUES
-(1, 'РќС–Р¶РёРЅ - РќРѕСЃС–РІРєР°', 2),
-(2, 'РќРѕСЃС–РІРєР°', 1),
-(3, 'РќС–Р¶РёРЅ', 1);
+(1, 'Ніжин - Носівка', 2),
+(2, 'Носівка', 1),
+(4, 'Ніжин', 1);
 
 -- --------------------------------------------------------
 
@@ -152,7 +133,7 @@ INSERT INTO `locality` (`id`, `locality`, `type_locality_id`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `measuringinstrument` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL,
   `type_measuring_instrument` varchar(45) NOT NULL,
   `serial_number` varchar(45) DEFAULT NULL,
   `inventory_number` varchar(45) DEFAULT NULL,
@@ -160,9 +141,8 @@ CREATE TABLE IF NOT EXISTS `measuringinstrument` (
   `number_certificate` varchar(45) NOT NULL,
   `calibration_interval` varchar(45) NOT NULL,
   `next_calibration` date NOT NULL,
-  `notices` varchar(1024) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `notices` varchar(1024) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -171,18 +151,17 @@ CREATE TABLE IF NOT EXISTS `measuringinstrument` (
 --
 
 CREATE TABLE IF NOT EXISTS `nameobject` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name_object` varchar(254) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+  `id` int(10) unsigned NOT NULL,
+  `name_object` varchar(254) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `nameobject`
 --
 
 INSERT INTO `nameobject` (`id`, `name_object`) VALUES
-(1, 'Р‘РѕРєРѕСЂС–Р·Рё'),
-(2, 'Р’РёРєСЂСѓС‚РєР°');
+(1, 'Бокорізи'),
+(2, 'Викрутка');
 
 -- --------------------------------------------------------
 
@@ -191,7 +170,7 @@ INSERT INTO `nameobject` (`id`, `name_object`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `object` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL,
   `type_object_id` int(10) unsigned NOT NULL,
   `tbl_name_object_id` int(10) unsigned NOT NULL,
   `tbl_brigada_id` int(10) unsigned NOT NULL,
@@ -201,14 +180,15 @@ CREATE TABLE IF NOT EXISTS `object` (
   `inspection_date` date NOT NULL,
   `inspection_interval` varchar(45) NOT NULL,
   `next_inspection` date NOT NULL,
-  `protocol_number` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_tbl_object_tbl_type_object1_idx` (`type_object_id`),
-  KEY `fk_tbl_object_tbl_facility1_idx` (`facility_id`),
-  KEY `fk_tbl_object_tbl_locality1_idx` (`tbl_locality_id`),
-  KEY `fk_tbl_object_tbl_brigada1_idx` (`tbl_brigada_id`),
-  KEY `fk_tbl_object_tbl_name_object1_idx` (`tbl_name_object_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+  `protocol_number` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `object`
+--
+
+INSERT INTO `object` (`id`, `type_object_id`, `tbl_name_object_id`, `tbl_brigada_id`, `inventory_number`, `tbl_locality_id`, `facility_id`, `inspection_date`, `inspection_interval`, `next_inspection`, `protocol_number`) VALUES
+(1, 2, 1, 4, NULL, 4, 4, '2015-12-20', '12', '2016-12-20', NULL);
 
 -- --------------------------------------------------------
 
@@ -217,18 +197,17 @@ CREATE TABLE IF NOT EXISTS `object` (
 --
 
 CREATE TABLE IF NOT EXISTS `typelocality` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `type_locality` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+  `id` int(10) unsigned NOT NULL,
+  `type_locality` varchar(45) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `typelocality`
 --
 
 INSERT INTO `typelocality` (`id`, `type_locality`) VALUES
-(1, 'РЎС‚Р°РЅС†С–СЏ'),
-(2, 'РџРµСЂРµРіС–РЅ');
+(1, 'Станція'),
+(2, 'Перегін');
 
 -- --------------------------------------------------------
 
@@ -237,21 +216,143 @@ INSERT INTO `typelocality` (`id`, `type_locality`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `typeobject` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `type_object` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+  `id` int(10) unsigned NOT NULL,
+  `type_object` varchar(45) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `typeobject`
 --
 
 INSERT INTO `typeobject` (`id`, `type_object`) VALUES
-(1, 'Р—Р°СЃС–Р± Р·Р°С…РёСЃС‚Сѓ'),
-(2, 'Р†РЅСЃС‚СЂСѓРјРµРЅС‚ Р· С–Р·РѕР»СЊРѕРІР°РЅРёР'),
-(3, 'Р—Р°СЃС–Р± Р·Р°С…РёСЃС‚Сѓ'),
-(4, 'Р†РЅСЃС‚СЂСѓРјРµРЅС‚ Р· С–Р·РѕР»СЊРѕРІР°РЅРёР');
+(1, 'Засіб захисту'),
+(2, 'Інструмент з ізольованими ручками');
 
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `brigada`
+--
+ALTER TABLE `brigada`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `certificate`
+--
+ALTER TABLE `certificate`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `facility`
+--
+ALTER TABLE `facility`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_tbl_facility_tbl_hazard_category_idx` (`hazard_category_id`);
+
+--
+-- Индексы таблицы `hazardcategory`
+--
+ALTER TABLE `hazardcategory`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `locality`
+--
+ALTER TABLE `locality`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_tbl_locality_tbl_type_locality_idx` (`type_locality_id`);
+
+--
+-- Индексы таблицы `measuringinstrument`
+--
+ALTER TABLE `measuringinstrument`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `nameobject`
+--
+ALTER TABLE `nameobject`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `object`
+--
+ALTER TABLE `object`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_tbl_object_tbl_type_object1_idx` (`type_object_id`),
+  ADD KEY `fk_tbl_object_tbl_facility1_idx` (`facility_id`),
+  ADD KEY `fk_tbl_object_tbl_locality1_idx` (`tbl_locality_id`),
+  ADD KEY `fk_tbl_object_tbl_brigada1_idx` (`tbl_brigada_id`),
+  ADD KEY `fk_tbl_object_tbl_name_object1_idx` (`tbl_name_object_id`);
+
+--
+-- Индексы таблицы `typelocality`
+--
+ALTER TABLE `typelocality`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `typeobject`
+--
+ALTER TABLE `typeobject`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `brigada`
+--
+ALTER TABLE `brigada`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT для таблицы `certificate`
+--
+ALTER TABLE `certificate`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=16;
+--
+-- AUTO_INCREMENT для таблицы `facility`
+--
+ALTER TABLE `facility`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT для таблицы `hazardcategory`
+--
+ALTER TABLE `hazardcategory`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT для таблицы `locality`
+--
+ALTER TABLE `locality`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT для таблицы `measuringinstrument`
+--
+ALTER TABLE `measuringinstrument`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `nameobject`
+--
+ALTER TABLE `nameobject`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT для таблицы `object`
+--
+ALTER TABLE `object`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT для таблицы `typelocality`
+--
+ALTER TABLE `typelocality`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT для таблицы `typeobject`
+--
+ALTER TABLE `typeobject`
+  MODIFY `id` int(10) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
@@ -272,11 +373,11 @@ ALTER TABLE `locality`
 -- Ограничения внешнего ключа таблицы `object`
 --
 ALTER TABLE `object`
-  ADD CONSTRAINT `fk_object_type_object` FOREIGN KEY (`type_object_id`) REFERENCES `typeobject` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_object_brigada` FOREIGN KEY (`tbl_brigada_id`) REFERENCES `brigada` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_object_facility` FOREIGN KEY (`facility_id`) REFERENCES `facility` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_object_locality` FOREIGN KEY (`tbl_locality_id`) REFERENCES `locality` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_object_brigada` FOREIGN KEY (`tbl_brigada_id`) REFERENCES `brigada` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_object_nameobject` FOREIGN KEY (`tbl_name_object_id`) REFERENCES `nameobject` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_object_nameobject` FOREIGN KEY (`tbl_name_object_id`) REFERENCES `nameobject` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_object_type_object` FOREIGN KEY (`type_object_id`) REFERENCES `typeobject` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
